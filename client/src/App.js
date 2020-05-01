4165785ee58ca6915ecaebcb2274d3fae50a9e36
Sahil Sindhu <sahil.sindhu@Sahils-MacBook-Pro.local>
2020-05-01 20:32:24 +0530

    React initial cleanup, register component, Login component markup added

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'; 
+import Navbar from '../src/components/layout/Navbar';
+import Landing from '../src/components/layout/Landing';
+import Login from '../src/components/auth/Login';
+import Register from '../src/components/auth/Register';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello react world</h1>
-    </div>
+    <Router>
+      <Fragment>
+        <Navbar />
+        <Route exact path='/' component= { Landing } />
+        <section className='container'>
+          <Switch>
+            <Route exact path='/login' component={ Login }/>
+            <Route exact path='/register' component={ Register }/>
+          </Switch>
+        </section>
+      </Fragment>
+    </Router>
   );
 }
 
